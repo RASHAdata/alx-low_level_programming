4bde383436c551ddb97fdb00addd5cbb5da25824
samira ait laasri <samira.aitlaasri@gmail.com>
2023-04-25 17:23:07 -0700

    104 fibonacci.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
- #include "main.h"
+#include "main.h"
 /**
  * print_last_digit - prints the last digit of a number
  * @n: the int to extract the last digit from
@@ -6,18 +6,18 @@
  */
 int print_last_digit(int n)
 {
-	int a;	
+	int a;
 
 	if (n < 0)
 		n = -n;
 
 	a = n % 10;
-		
+
 	if (a < 0)
 		a = -a;
-	
+
 	_putchar(a + '0');
-	
+
 	return (a);
-		
+
 }
