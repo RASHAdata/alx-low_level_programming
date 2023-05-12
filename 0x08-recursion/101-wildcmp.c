89f2e52632d36554a07e7cc2ec0374598fb90a29
samira ait laasri <samira.aitlaasri@gmail.com>
2023-05-11 21:31:33 -0700

    ok

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,7 +6,6 @@
  * @s2: pointer to string params
  * Return: 0
  */
-
 int wildcmp(char *s1, char *s2)
 {
     if (*s1 == '\0')
@@ -19,11 +18,11 @@ int wildcmp(char *s1, char *s2)
     }
     if (*s2 == '*')
     {
-    return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+    return (wildcmp(s1 + 1, s2)||wildcmp(s1, s2 + 1));
     }
     else if (*s1 == *s2)
     {
-    return (wildcmp(s1 + 1, s2 + 1));
+	    return (wildcmp(s1 + 1, s2 + 1));
     }
-    return (0);
+return (0);
 }
